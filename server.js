72ab7abc69efde6ed0aabe369265f4e6de4c2ab7
Miller Rich <william.miller.rich@gmail.com>
2020-10-08 09:57:28 -0500

    add index.js file to models folder and export Workout, require models directory in server for const db

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
-const Workout = require("./models/workout");
+const db = require("./models");
 
 const PORT = process.env.PORT || 3000;
 
