e7c43fc2bb21ffc154a81d133d7d7c613ae3c3f9
Miller Rich <william.miller.rich@gmail.com>
2020-10-07 22:29:34 -0500

    create workoutSchema in workout.js in models directory, export as Workout

---
+++ models/workout.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    exercises: [{
+        type: String,
+        name: String,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number
+    }]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
