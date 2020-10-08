52f3cc5274b5a980449f5199bd14e5258f9b0019
Miller Rich <william.miller.rich@gmail.com>
2020-10-07 21:50:30 -0500

    set up html routes in server.js, require path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.get("/stats", (req, res) =>{
+    res.sendFile(path.join(__dirname, "/public/stats.html"));
+});
+
+app.get("/exercise", (req, res) =>{
+    res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
 
 app.listen(PORT, () => {
     console.log("listening at http://localhost:" + PORT);
